5a71ac0fb3df0e84bb16c551c82e0a4bbb35e78d
issacbelike-max <issacbelike@gmail.com>
2026-07-05 18:08:50 +0700

    Delete Web Project directory

--- Web Project/web.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-web = Flask(__name__)
-
-@web.route("/")
-def home():
-    return render_template("index.html")
-
-if __name__ == "__main__":
-    web.run(debug=True)
